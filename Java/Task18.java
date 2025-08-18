1e0e9e274ce940e30cc7245a7865fc7644c6ecda
SadiqIbrahim01 <164149043+SadiqIbrahim01@users.noreply.github.com>
2025-08-18 07:55:24 -0700

    Add files via upload

---
+++ Java/Task18.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Task18{
+public static void main(String[] args){
+	for (int num = 1; num <= 100; num++){
+		if (num % 3 == 0 && num % 5 == 0){
+			System.out.println("FizzBuzz");
+		}
+		else if (num % 3 == 0){
+			System.out.println("Fizz");
+		}
+		else if (num % 5 == 0){
+			System.out.println("Buzz");
+		}
+	}
+  }
+}
