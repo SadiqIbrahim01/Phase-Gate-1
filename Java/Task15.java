c68a6e2426a9cb99db7c101d5ef3677c3f8e1910
SadiqIbrahim01 <164149043+SadiqIbrahim01@users.noreply.github.com>
2025-08-18 07:47:54 -0700

    Add files via upload

---
+++ Java/Task15.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Task15{
+public static void main(String[] args){
+	int total = 0;
+	for (int num = 1; num <= 100; num++){
+		total += num;
+	}
+	System.out.println(total);
+  }
+}
